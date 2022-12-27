b19615fd926c3a63a5d98d9b7aa5965dd83e1e20
ayho <y.houssin01@gmail.com>
2022-12-27 01:48:19 +0100

    test

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,20 @@ const { locale } = useI18n();
 onMounted(() => {
   setTheme();
   locale.value = localStorage.getItem("lang") || "fr";
+  async function detectSWUpdate() {
+    const registration = await navigator.serviceWorker.ready;
+
+    registration.addEventListener("updatefound", event => {
+      alert('updateFound')
+      const newSW = registration.installing!;
+      newSW.addEventListener("statechange", event => {
+        if (newSW.state == "installed") {
+          // New service worker is installed, but waiting activation
+        }
+      });
+    })
+  }
+  detectSWUpdate();
 });
 </script>
 
