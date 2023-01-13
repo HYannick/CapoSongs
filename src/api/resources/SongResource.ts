05be1b2f16d2e9ad1994f06a79330ecfa84ad18b
ayho <y.houssin01@gmail.com>
2023-01-13 14:57:59 +0100

    bind search with backend and add loader

--- src/api/resources/SongResource.ts
+++ src/api/resources/SongResource.ts
@@ -3,7 +3,6 @@ import type { RestSongData } from "@/api/rest/RestSong";
 import { toSong } from "@/api/rest/RestSong";
 import { AuthorizationHeader } from "@/api/auth-headers";
 import { SongsNotFoundException } from "@/common/domain/SongsNotFoundException";
-import { paramsSerializer } from "@/api/paramsSerializer";
 
 export const songResource = () => {
   const getSongs = async (
@@ -11,20 +10,30 @@ export const songResource = () => {
     searchQuery?: string
   ): Promise<Song[]> => {
     try {
-      const query = paramsSerializer({
-        filters: {
-          title: {
-            $containsi: searchQuery,
-          },
-        },
-        populate: "*",
-      });
       const res = await fetch(
-        `${import.meta.env.VITE_STRAPI_API_URL}/songs?${query}`,
+        `${import.meta.env.VITE_STRAPI_API_URL}/songs?${
+          searchQuery ? `filters[title][$containsi]=${searchQuery}&` : ""
+        }populate=*`,
         {
           ...AuthorizationHeader(import.meta.env.VITE_STRAPI_TOKEN),
         }
       );
+
+      //TODO replace above by this
+      // const query = paramsSerializer({
+      //   filters: {
+      //     title: {
+      //       $containsi: searchQuery,
+      //     },
+      //   },
+      //   populate: "*",
+      // });
+      // const res = await fetch(
+      //   `${import.meta.env.VITE_STRAPI_API_URL}/songs?${query}`,
+      //   {
+      //     ...AuthorizationHeader(import.meta.env.VITE_STRAPI_TOKEN),
+      //   }
+      // );
       const response: RestSongData = await res.json();
       return response.data.map((restSong) => toSong(restSong));
     } catch (e) {
