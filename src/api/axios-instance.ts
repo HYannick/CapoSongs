6445f1e9dce2be6d69c000998868149f8ddf48a0
ayho <y.houssin01@gmail.com>
2023-01-13 02:45:06 +0100

    huge changes on api handling

--- src/api/axios-instance.ts
+++
@@ -1,7 +0,0 @@
-import axios from "redaxios";
-import { paramsSerializer } from "@/api/paramsSerializer";
-
-export const backendAxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_STRAPI_API_URL,
-  paramsSerializer,
-});
--- src/api/resources/SongResource.ts
+++ src/api/resources/SongResource.ts
@@ -3,20 +3,20 @@ import type { RestSongData } from "@/api/rest/RestSong";
 import { toSong } from "@/api/rest/RestSong";
 import { AuthorizationHeader } from "@/api/auth-headers";
 import { SongsNotFoundException } from "@/common/domain/SongsNotFoundException";
+import { paramsSerializer } from "@/api/paramsSerializer";
+
+const SONGS_URL = (queryParams: string) =>
+  `${import.meta.env.VITE_STRAPI_API_URL}/songs?${queryParams}`;
 
 export const songResource = () => {
   const getSongs = async (pagination: { page: number }): Promise<Song[]> => {
     try {
-      const res = await fetch(
-        `${import.meta.env.VITE_STRAPI_API_URL}/songs?populate=*`,
-        {
-          ...AuthorizationHeader(import.meta.env.VITE_STRAPI_TOKEN),
-        }
-      );
+      const res = await fetch(SONGS_URL(paramsSerializer({ populate: "*" })), {
+        ...AuthorizationHeader(import.meta.env.VITE_STRAPI_TOKEN),
+      });
       const response: RestSongData = await res.json();
       return response.data.map((restSong) => toSong(restSong));
     } catch (e) {
-      console.log(e);
       throw new SongsNotFoundException();
     }
   };
