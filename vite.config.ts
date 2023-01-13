f16d15a80ba836917cee1151a396c8726739b304
ayho <y.houssin01@gmail.com>
2023-01-13 02:02:41 +0100

    huge changes on api handling

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,6 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ["axios", "qs"],
-    },
-  },
   plugins: [
     vue(),
     svgLoader(),
