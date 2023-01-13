7d519e4d872db6c20f4865635895124c79b25d71
ayho <y.houssin01@gmail.com>
2023-01-13 01:44:35 +0100

    huge changes on api handling

--- src/utils/axios-instance.ts
+++ src/utils/axios-instance.ts
@@ -1,4 +1,4 @@
-const axios = require("axios").default;
+import axios from "axios";
 import { paramsSerializer } from "@/utils/paramsSerializer";
 
 export const backendAxiosInstance = axios.create({
