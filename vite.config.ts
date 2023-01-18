dc014a94e66b915b5d757d3bed815a0b1a2a1c2b
ayho <y.houssin01@gmail.com>
2023-01-18 01:00:00 +0100

    adding filters

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: "esnext",
+  },
   plugins: [
     vue(),
     svgLoader(),
