7fb0145d0c12ff5ee52dd050dbb777152e523447
ayho <y.houssin01@gmail.com>
2023-01-18 01:12:01 +0100

    adding filters

---
+++ src/api/qs/index.js
@@ -0,0 +1,9 @@
+import stringify from "./stringify";
+import parse from "./parse";
+import formats from "./formats";
+
+export default {
+  formats: formats,
+  parse: parse,
+  stringify: stringify,
+};
